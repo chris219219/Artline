d2831c6d3558e63c5596a323b6d071a4de86a249
chris219219 <ctan0619@gmail.com>
2023-10-20 08:32:27 -0400

    added client datagrid, binding for buttons

--- ViewModels/DataPageViewModel.cs
+++ ViewModels/DataPageViewModel.cs
@@ -1,8 +1,11 @@
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Artline.Models;
 using Avalonia.Collections;
+using Avalonia.Controls;
 using Avalonia.Platform;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Artline.ViewModels;
 
@@ -14,4 +17,16 @@ public partial class DataPageViewModel : ViewModelBase
     }
 
     public Database Database { get; }
+
+    [ObservableProperty]
+    private bool _editProjectButtonEnabled = false;
+
+    [ObservableProperty]
+    private bool _deleteProjectButtonEnabled = false;
+
+    [ObservableProperty]
+    private bool _editClientButtonEnabled = false;
+
+    [ObservableProperty]
+    private bool _deleteClientButtonEnabled = false;
 }
--- Views/DataPageView.axaml.cs
+++ Views/DataPageView.axaml.cs
@@ -12,4 +12,21 @@ public partial class DataPageView : UserControl
         InitializeComponent();
     }
 
+    private void OnProjectSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (DataContext is DataPageViewModel vm)
+        {
+            vm.EditProjectButtonEnabled = e.AddedItems.Count == 1;
+            vm.DeleteProjectButtonEnabled = e.AddedItems.Count > 0;
+        }
+    }
+
+    private void OnClientSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (DataContext is DataPageViewModel vm)
+        {
+            vm.EditClientButtonEnabled = e.AddedItems.Count == 1;
+            vm.DeleteClientButtonEnabled = e.AddedItems.Count > 0;
+        }
+    }
 }
